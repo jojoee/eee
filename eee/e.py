9cca820fbc18be5f583c474a10e7f471c30bc228
Mrmagicpie <70033286+Mrmagicpie@users.noreply.github.com>
2021-04-23 13:42:39 -0400

    Update e.py

--- eee/e.py
+++ eee/e.py
@@ -1,2 +1,21 @@
-def e(y=3):
+
+def e(y: int = 3, makeList: bool = False):
+    if makeList:
+        return ["e" for e in range(y)]
     return "e" * y
+
+def into_e(input):
+    input_type = type(input)
+    
+    if input_type is str:
+        return e(len(input))
+    
+    elif input_type is list:
+        return e(len(input), makeList = True)
+
+    elif input_type is tuple:
+        return tuple(e(len(input), makeList = True))
+    
+    else:
+        raise ValueError("Unsupported input type!")
+        
