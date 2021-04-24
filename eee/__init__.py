0a295291e5d50959fb0d435bd8627145ce80df9e
Nathachai Thongniran <inid3a@gmail.com>
2021-04-24 08:55:49 +0700

    style: format style and remove unused

--- eee/__init__.py
+++ eee/__init__.py
@@ -4,8 +4,6 @@ eee ee eeeee
     >>> e(10)
 """
 
-# import eee
-# eee.__version__
 __version__ = "0.0.3"
 
 from eee.e import e, into_e
--- eee/e.py
+++ eee/e.py
@@ -1,21 +1,20 @@
-
 def e(y: int = 3, makeList: bool = False):
     if makeList:
         return ["e" for e in range(y)]
     return "e" * y
 
+
 def into_e(input):
     input_type = type(input)
-    
+
     if input_type is str:
         return e(len(input))
-    
+
     elif input_type is list:
-        return e(len(input), makeList = True)
+        return e(len(input), makeList=True)
 
     elif input_type is tuple:
-        return tuple(e(len(input), makeList = True))
-    
+        return tuple(e(len(input), makeList=True))
+
     else:
         raise ValueError("Unsupported input type!")
-        
